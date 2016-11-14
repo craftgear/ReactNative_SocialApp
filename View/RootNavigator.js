c40d9abaed08f52ae1973e1e90cda9dde0b02597
Shunsuke Watanabe <craftgear@gmail.com>
2016-11-14 19:58:10 +0900

    add navigator

--- View/RootNavigator.js
+++ View/RootNavigator.js
@@ -3,6 +3,15 @@ import { Navigator, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default class RootNavigator extends Component {
   render() {
-    return <View></View>
+    return (
+      <Navigator
+        ref={(navigator) => this._setNavigatorRef(navigator)}
+        initialRoute={this._getInitialRoute()}
+        renderScene={(route, navigator) => this.renderScene(route, navigator)}
+        navigationBar={this.state.hideNavigationBar ? null : nav}
+      >
+
+      </Navigator>
+    )
   }
 }
